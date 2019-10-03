2ae19889913d9fc3087c3a3005071fe7b672e584
Werstern <wersternn@gmail.com>
2019-10-03 15:49:22 +0300

    adding basic validation

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -5,6 +5,7 @@ import {
   Text, 
   TextInput,
   Platform,
+  Alert,
   StyleSheet 
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -24,6 +25,7 @@ const EditProductScreen = props => {
   const [title, setTitle] = useState(
     editedProduct ? editedProduct.title : ''
   );
+  const [titleIsValid, setTitleIsValid] = useState(false);
   const [imageUrl, setImageUrl] = useState(
     editedProduct ? editedProduct.imageUrl : ''
   );
@@ -33,6 +35,12 @@ const EditProductScreen = props => {
   );
 
   const submitHandler = useCallback(() => {
+    if (!titleIsValid) {
+      Alert.alert('Wrong input!', 'Please check the errors in the form.', [
+        { text: 'Okay' }
+      ]);
+      return;
+    }
     if (editedProduct) {
       dispatch(
         productsActions.updateProduct(prodId, title, description, imageUrl)
@@ -49,6 +57,15 @@ const EditProductScreen = props => {
     props.navigation.setParams({ submit: submitHandler });
   }, [submitHandler]);
 
+  const titleChangeHandler = text => {
+    if (text.trim().length === 0) {
+      setTitleIsValid(false);
+    } else {
+      setTitleIsValid(true);
+    }
+    setTitle(text);
+  };
+
   return (
     <ScrollView>
       <View style={styles.form}>
@@ -57,8 +74,13 @@ const EditProductScreen = props => {
           <TextInput 
             style={styles.input} 
             value={title}
-            onChangeText={text => setTitle(text)}
+            onChangeText={titleChangeHandler}
+            keyboardType='default'
+            autoCapitalize='sentences'
+            autoCorrect
+            returnKeyType='next'
           />
+          {!titleIsValid && <Text>Please enter a valid title!</Text>}
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Image URl</Text>
@@ -74,7 +96,8 @@ const EditProductScreen = props => {
             <TextInput 
               style={styles.input}
               value={price}
-              onChangeText={text => setPrice(text)} 
+              onChangeText={text => setPrice(text)}
+              keyboardType='decimal-pad' 
             />
           </View>
         )}
