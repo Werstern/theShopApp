112b5d9b03162bdce24b02535b393936a9814ffb
Werstern <wersternn@gmail.com>
2019-10-07 13:31:04 +0300

    fetching stored orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,6 +1,43 @@
-export const ADD_ORDER = 'ADD_ORDER';
+import Order from "../../models/order";
 
-export const addOrder = (cartItems, totalAMount) => {
+export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        'https://shopapp-336dc.firebaseio.com/orders/u1.json'
+      );
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+      dispatch({ 
+        type: SET_ORDERS, 
+        orders: loadedOrders 
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
     const date = new Date();
     const response = await fetch(
@@ -12,7 +49,7 @@ export const addOrder = (cartItems, totalAMount) => {
         },
         body: JSON.stringify({
           cartItems,
-          totalAMount,
+          totalAmount,
           date: date.toISOString()
         })
       }
