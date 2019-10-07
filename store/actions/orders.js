e7d3e1e75da87639dedd0861045bc94a09747d28
Werstern <wersternn@gmail.com>
2019-10-07 12:38:34 +0300

    storing orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,11 +1,37 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAMount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: {
-      items: cartItems,
-      amount: totalAMount
+  return async dispatch => {
+    const date = new Date();
+    const response = await fetch(
+      'https://shopapp-336dc.firebaseio.com/orders/u1.json', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAMount,
+          date: date.toISOString()
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
     }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAMount,
+        date: date
+      }
+    });
   };
 };
