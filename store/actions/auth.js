8c6dffdce024f5010b991858428ca88a33c772f5
Werstern <wersternn@gmail.com>
2019-10-07 16:41:31 +0300

    adding user signup

---
+++ store/actions/auth.js
@@ -0,0 +1,28 @@
+export const SIGNUP = 'SIGNUP';
+
+export const signUp = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDPG55dC9umc3elAgXMKSw_qIE6nnJbZnM',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
+};
